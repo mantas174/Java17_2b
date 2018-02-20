4abb93cd43db6950f33464e8b0434a3c5f7c19af
Mantas <mantas.muzzz@gmail.com>
2018-02-20 09:45:43 +0200

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// 2b.
+        //​
+        // Parašyti programą, kuri paprašytų vesti skaičius tol, kol bus įvestas skaičius 0. Pabaigoje
+        //turi būti atvaizduojama įvestų skaičių suma.
+
+
+    }
+}
